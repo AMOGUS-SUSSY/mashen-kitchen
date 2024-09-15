0c4ed514ed71ef102196eb50297ac0d5716f7a79
mashen <mashen@student.ethz.ch>
2024-09-15 23:22:04 +0200

    Get tokens directly

--- tokens.py
+++ tokens.py
@@ -5,14 +5,14 @@ from tqdm import tqdm
 from collections import Counter
 
 # RUN FROM MASHEN KITCHEN
-IN = "data/mashen/pre_16284"
+VOCAB = "languini/vocabs/spm_models/"
 
 def count(file):
     tokens = np.load(file)
     c = Counter(tokens)
     return c
 
-def main(data_root,vocab_size, output):
+def main(data_root,vocab,vocab_size, output):
     c = Counter()
     print("Counting...")
     for root, dirs, files in os.walk(data_root):
@@ -20,17 +20,21 @@ def main(data_root,vocab_size, output):
             c.update(count(os.path.join(data_root,file)))
     print("Writing...")
     open(output, 'w')
+    vocab = open(VOCAB+vocab, 'r')
+    lines = vocab.readlines()
     with open(output, 'a') as out:
         for element in tqdm(c.most_common(vocab_size)):
-            out.write(','.join([str(x) for x in element]) + '\n')
+            temp = [x for x in element]
+            out.write(lines[temp[0]] + "," + str(temp[1]))
     print("DONE")
 
 if __name__ == "__main__":
     # Use argparse to get command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("--data_root", required=True, help="path to all the tokenised files")
+    parser.add_argument("--vocab", required=True, help="vocab-name")
     parser.add_argument("--vocab_size", required=True, type=int, help="vocabulary size")
     parser.add_argument("--output", required=True, help="file to write stuff into")
     args = parser.parse_args()
 
-main(args.data_root, args.vocab_size, args.output)
+main(args.data_root, args.vocab, args.vocab_size, args.output)
